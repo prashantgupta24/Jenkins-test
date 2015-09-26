7e8816329307a3e98404aca09f864750625c82ca
Prashant Gupta <prashantgupta24@gmail.com>
2015-09-26 19:16:35 -0400

    made some changes

--- test1.js
+++ test1.js
@@ -3,7 +3,7 @@ var express = require('express')
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('hello changgddd');
+  res.send('hi there');
 });
 
 app.listen(process.env.port || 5000);
