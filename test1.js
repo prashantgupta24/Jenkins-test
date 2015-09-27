01a4655032d195bc76e31510161e2a54cf3437a4
Prashant Gupta <prashantgupta24@gmail.com>
2015-09-27 16:45:48 -0400

    Update test1.js

--- test1.js
+++ test1.js
@@ -3,7 +3,7 @@ var express = require('express')
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('hi there');
+  res.send('hi theres');
 });
 
 app.listen(process.env.port || 5000);
